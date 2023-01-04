15b0b75276615337d9579b526d629986d00ab8c0
menchaca-cherie <menchaca.cherie@student.greenriver.edu>
2023-01-03 23:15:42 -0800

    Added an equals() and ahshcode() method.

--- Test Project/src/making_changes/Point.java
+++ Test Project/src/making_changes/Point.java
@@ -1,5 +1,7 @@
 package making_changes;
 
+import java.util.Objects;
+
 public class Point
 {
     private double x;
@@ -27,6 +29,19 @@ public class Point
 
     public void setY(double y) {
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 
     @Override
